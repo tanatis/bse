8261022cf97909506a12b7a365e09a18bc6910dd
Ivan Pekov <64958860+tanatis@users.noreply.github.com>
2023-12-08 19:51:39 +0200

    working on templates

--- bse/portfolio/views.py
+++ bse/portfolio/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, CreateView, DeleteView
 
 from bse.portfolio.models import Portfolio
-from bse.positions.models import Position
+from bse.positions.models import Position, PositionHistory
 
 
 class PortfolioDetailsView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
@@ -16,7 +16,22 @@ class PortfolioDetailsView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         positions = Position.objects.filter(to_portfolio_id=self.object.pk)
+        positions_value = 0
+        for position in positions:
+            position.history = PositionHistory.objects.filter(to_position=position)
+            position.value = position.count * position.avg_price
+            positions_value += position.value
+        portfolio = self.get_object()
+        portfolio.cash = portfolio.cash
+        total = portfolio.cash + positions_value
+        portfolio.cash_percentage = portfolio.cash / total * 100
+
+        for position in positions:
+            position.percentage = position.value / total * 100
         context['positions'] = positions
+        context['total'] = total
+        context['cash_percentage'] = portfolio.cash_percentage
+
         return context
 
     def test_func(self):
